69a42a11a159778df800baaa9fe93eaa5252ddad
Cedric <cedric.lor@gmail.com>
2015-12-15 15:38:47 +0100

    reducing languages assets for CKEditor

---
+++ config/initializers/ckeditor.rb
@@ -0,0 +1,5 @@
+Ckeditor.setup do |config|
+  config.assets_languages = ['en', 'fr', 'ru', 'zh-CN']
+  # config.assets_plugins = ['liststyle', 'image', 'smiley', 'a11yhelp', 'forms', 'colordialog', 'find', 'clipboard', 'specialchar', 'dialog', 'about', 'iframe', 'tabletools', 'wsc', 'pastefromword', 'div', 'table', 'scayt', 'templates', 'flash', 'link', 'wsc', 'icons_hidpi', 'magicline', 'showblocks', 'icons', 'pagebreak']
+end
+
