fee18b4141469edc28d071a464951160c4d86ad8
Cedric <cedric.lor@gmail.com>
2016-02-22 19:30:07 +0100

    Pure Rails: modified the queries in article and aktion model to let the articles with the correct status appear on the front page

--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -25,11 +25,7 @@ class HomePage < ActiveRecord::Base
 
   def short_editorial
     end_of_short_edito = editorial.index('</p>', 350) + 4
-    short_editorial = editorial.slice(0, end_of_short_edito)
-    if editorial.size > end_of_short_edito
-      short_editorial = short_editorial + '...'
-    end
-    short_editorial
+    editorial.slice(0, end_of_short_edito)
   end
 end
 
