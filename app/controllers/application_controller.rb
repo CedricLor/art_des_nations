535fc647d8fcb536051bcff7c13980a72c61047c
Cedric <cedric.lor@gmail.com>
2016-02-22 22:33:39 +0100

    Pure Rails -- Heavy refactoring of the home_pages partrails to avoid errors when cards do not have images; same on categories, portraits and country partials ; refacto language switcher; fixed author bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :set_categories_for_side_nav
   before_action :set_countries_for_side_nav
-  before_action :set_remaining_locales_for_language_switcher
+  before_action :set_locales_for_language_switcher
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -26,10 +26,7 @@ class ApplicationController < ActionController::Base
     @countries = Country.includes(:translations).all
   end
 
-  def set_remaining_locales_for_language_switcher
-    language_array = [{fr: "FR"}, {en: "EN"}, {ru: "PY"}, {:"zh-CN" => "中文"}]
-    @remaining_locales = language_array.select do |available_locale|
-      available_locale unless available_locale.keys[0] == I18n.locale
-    end
+  def set_locales_for_language_switcher
+    @languages_array = [{fr: "FR"}, {en: "EN"}, {ru: "PY"}, {:"zh-CN" => "中文"}]
   end
 end
--- app/forms/article_update_form.rb
+++ app/forms/article_update_form.rb
@@ -57,12 +57,13 @@ class ArticleUpdateForm
     # FIXME -- We need some kind of validation rule before executing this
     # if nothing has changed, return true
     return true if @article.author.full_name == create_new_author && @article.author_id.to_s == author_id
+    # else if the user has entered a new name in the input field, create author and assign it the article
+    # if the user has both changed the author from the list and entered a new name in the field,
+    # we skip the registration of another author from the list
+    return create_author_and_assign_article if create_new_author.present?
     # else if the user has selected a different author in the list, update the article and return
     return @article.update(author_id: author_id) if @article.author_id.to_s != author_id
-    # else if the user has entered a new name in the input field, create author and assign it the article
-    # if he user has both changed the article from the list and entered a new name in the field,
-    # we will not register the new author
-    create_author_and_assign_article if create_new_author.blank?
+    byebug
   end
 
   def create_author_and_assign_article
