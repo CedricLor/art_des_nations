bee45318e570da22d654d7b5650033020d294237
Cedric <cedric.lor@gmail.com>
2016-02-22 20:16:18 +0100

    Pure Rails: fixed bug when article does not have any picture, it should still display on the front page

--- app/models/aktion.rb
+++ app/models/aktion.rb
@@ -40,6 +40,19 @@ class Aktion < ActiveRecord::Base
 
   translates :title, :body, :teaser, :status, :fallbacks_for_empty_translations => true
 
+  # def self.for_country(country)
+  #   Aktion.includes(picturizings: [:translations, media_container: :translations]).
+  #     where(status: ["published", "featured"]).
+  #     where(country_id: country.id)
+  # end
+
+  # def self.for_home_page
+  #   Aktion.includes(country: :translations).
+  #     includes(categorizings: [category: :translations]).
+  #     includes(picturizings: [:translations, media_container: :translations]).
+  #     where(status: ["published", "featured"])
+  # end
+
   def self.for_country(country)
     Aktion.where(status: ["published", "featured"]).
       where(country_id: country.id).
