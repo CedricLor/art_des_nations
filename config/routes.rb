69032e3a4dafcf0edcc42fa059ad914776bff0b1
Cedric <cedric.lor@gmail.com>
2016-02-24 00:58:17 +0100

    Pure Rails: removed devise registerable; added routes to edit profile and update profile in routes.rb; added a mini admin nav at the bottom of the page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   scope '(:locale)', locale: /fr|en|ru|zh-CN/ do
     # FIXME -- This was for React
     # get 'article/:id' => "pages#show"
