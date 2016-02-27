20a5c5c74a31c3d0b2dc9b7fa7976d7dc958b1f1
Cedric <cedric.lor@gmail.com>
2016-02-27 14:24:54 +0100

    Pure Rails: fixing the conflicts issue between locales and ids for static_pages; reverting to a route to page path; BIS

--- config/routes.rb
+++ config/routes.rb
@@ -33,8 +33,7 @@ Rails.application.routes.draw do
     # FIXME -- This was the React default route
     # root to: "pages#show"
     resources :static_pages, only: [:show, :edit, :update], path: 'page'
-    # get 'page/:id', to: 'static_pages#show', as: 'page'
-    get ':id', to: 'static_pages#show', as: ''
+    get 'page/:id', to: 'static_pages#show', as: 'page'
     resources :home_pages, only: [:show, :edit, :update]
     resources :external_links, only: [:create]
     resources :site_editorials
