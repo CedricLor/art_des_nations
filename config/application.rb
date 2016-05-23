ce81c041dbc88d41ffd91abd6bb74130622f8a4d
Cedric <cedric.lor@gmail.com>
2016-05-24 00:07:55 +0200

    Adding rack deflater

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module ArtDesNations
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.middleware.use Rack::Deflater
+
     # Include modules
     config.autoload_paths += %W(#{config.root}/lib/my_modules)
   end
