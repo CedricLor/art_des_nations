7db719c48f308e7d5eadbb81d380a25445f82468
Cedric <cedric.lor@gmail.com>
2016-05-23 23:48:14 +0200

    Removing active model serializer initializer

--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,5 +1,5 @@
 # active_model_serializer.rb
 # ActiveModel::Serializer.config do |config|
-#   # config.embed = :ids
-#   # config.embed_in_root = true
+#   config.embed = :ids
+#   config.embed_in_root = true
 # end
